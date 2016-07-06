e78a28b9b4515426a266e115e78992d6ae46e855
Ha inKi <vkhaiha@gmail.com>
2016-07-06 10:29:54 -0400

    First commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:8080;dbname=inki',
+    'username' => 'root',
+    'password' => '2861995',
+    'charset' => 'utf8',
+];
